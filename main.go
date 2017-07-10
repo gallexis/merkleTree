ae0acd6ad2416c4f7a836135793f912c51eb6bdf
alexisgallepe <alexis.gallepe@gmail.com>
2017-07-10 23:37:00 +0200

    Implemented GetRoot(), GetHeight() & GetLevel(), but the errors are not yet handled

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"crypto/sha256"
 )
 
 type merkleNode struct {
@@ -12,90 +13,105 @@ type merkleNode struct {
 	rightChild *merkleNode
 }
 
-
 func CreateMerkleTree(values []string)  merkleNode {
-
-
 	length := len(values)
 	heightTree := math.Ceil(math.Log2(float64(length)))
-	fmt.Println(heightTree)
 
-	t := createTree(int(heightTree), values)
-
-	fmt.Println(t)
-	fmt.Println(t.leftChild)
-	fmt.Println(t.leftChild.leftChild)
-	fmt.Println(t.leftChild.rightChild)
-	fmt.Println(t.rightChild)
-	fmt.Println(t.rightChild.leftChild)
-	fmt.Println(t.rightChild.rightChild)
-
-	return t
+	return *createTree(int(heightTree), values)
 }
 
-func  createTree(level int, hashes []string) merkleNode{
-	lenghtHashes := len(hashes)
+
+/*
+	1.				[1,2,3,4,5]
+
+
+	2.				     []
+			[1,2,3,4]		   [5]
+
+
+	3.						        []
+					[]		                      []
+			[1,2]        [3,4]                   [5]
+
+
+	4.							[]
+				   []		                 []
+			[]	          []                 []
+		[1]	  [2]     [3]   [4]             [5]
+ */
+func createTree(level int, hashes []string) *merkleNode{
+	lengthHashes := len(hashes)
+	halfLengthHashes := 0
 
 	if level == 0{
-		if lenghtHashes == 1{
-			return createLeaf(hashes[0])
+		if lengthHashes == 1{
+			leaf := merkleNode{isLeaf:true, hash: "", leftChild: nil, rightChild: nil}
+			leaf.hashMe(hashes[0])
+			return &leaf
 		}else {
-			return createLeaf("emptyleaf")
+			leaf :=  merkleNode{isLeaf:true, hash: "", leftChild: nil, rightChild: nil}
+			return &leaf
 		}
 
 	} else if level > 0{
 		level -= 1
-		node := createNode("")
-		h := lenghtHashes % 2
+		isNumberOfHashesEven := (lengthHashes % 2) == 0
+		node := merkleNode{isLeaf:false, hash: "", leftChild: nil, rightChild: nil}
 
-		if h == 1{
-			h = (lenghtHashes / 2) + 1
+		if isNumberOfHashesEven{
+			halfLengthHashes = (lengthHashes / 2)
 		}else{
-			h = (lenghtHashes / 2)
+			halfLengthHashes = (lengthHashes / 2) + 1
 		}
 
-		lc := createTree(level,hashes[:h])
-		rc := createTree(level,hashes[h:])
-		node.leftChild  = &lc
-		node.rightChild = &rc
-		//node.hash = hash256(node.leftChild.hash + node.rightChild.hash)
-		return node
+		node.leftChild  = createTree(level,hashes[:halfLengthHashes])
+		node.rightChild = createTree(level,hashes[halfLengthHashes:])
+
+		if node.rightChild.hash != ""{
+			node.hashMe(node.leftChild.hash + node.rightChild.hash)
+		}else{
+			node.hashMe(node.leftChild.hash)
+		}
+
+		return &node
 
 	} else {
 		fmt.Println("erreur")
-		return merkleNode{}
+		return &merkleNode{}
 	}
 }
 
-
-func  createLeaf(hsh string) merkleNode{
-	return merkleNode{isLeaf:true, hash: hsh, leftChild: nil, rightChild: nil}
+func (node *merkleNode) hashMe(data string){
+	sum := sha256.Sum256([]byte(data))
+	node.hash = fmt.Sprintf("%x", sum)
 }
 
-func  createNode(hsh string) merkleNode{
-	return merkleNode{isLeaf:false, hash: hsh, leftChild: nil, rightChild: nil}
-}
-
-func (node *merkleNode) addData(str string){
-
-}
-
-func (node merkleNode) root() (string){
+func (node merkleNode) GetRoot() (string){
 	return node.hash
 }
 
-func (node *merkleNode) height() (i int){
-	for i=1; !node.leftChild.isLeaf ;i++{
+func (node *merkleNode) GetHeight() (i int){
+	for i=0; !node.isLeaf ;i++{
 		node=node.leftChild
 	}
 	return
 }
 
-func (node *merkleNode) level(index int){
+func (node *merkleNode) GetLevel(level int) []string{
+
+	if(level <= 0){
+		return []string{node.hash}
+	} else{
+		return append( node.leftChild.GetLevel(level-1), node.rightChild.GetLevel(level-1) ... )
+	}
 
 }
 
+
 func main(){
-	t := CreateMerkleTree([]string{"aa","bb","cc","ccd"})
-	fmt.Println(t.height())
+	t := CreateMerkleTree([]string{"a", "b","c","d","e"})
+
+	fmt.Println(t.GetRoot())
+	fmt.Println(t.GetHeight())
+	fmt.Println(t.GetLevel(3))
 }
